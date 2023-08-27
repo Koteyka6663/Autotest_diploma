0056229911635f97eb318a67c971b8c05057b0dd
Koteyka6663 <mihlaw112@gmail.com>
2023-08-27 16:41:10 +0300

    first push

---
+++ configuration.py
@@ -0,0 +1,10 @@
+# Путь к серверу
+URL_SERVICE = "https://0ebca73c-b358-4dc6-8152-cad0e8583e23.serverhub.praktikum-services.ru"
+
+
+# Путь на создание заказа
+CREATE_ORDER = "/api/v1/orders"
+
+
+# Путь получения информации о заказе
+GET_ORDER_PATH = "/api/v1/orders/track"
---
+++ data.py
@@ -0,0 +1,14 @@
+# Тело запроса для создания заказа
+order_body = {
+                "firstName": "Котейка",
+                "lastName": "Котейков",
+                "address": "Пёсвиль",
+                "metroStation": 42,
+                "phone": "+79660561340",
+                "rentTime": 5,
+                "deliveryDate": "2023-06-06",
+                "comment": "Мур",
+                "color": [
+                          "BLACK"
+                         ]
+             }
